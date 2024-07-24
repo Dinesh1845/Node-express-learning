ba49ea4fdb98e9b5f120470ff946f436bac88efb
DineshShankar <dineshshankar156@gmail.com>
2024-07-24 21:38:29 +0530

    l2

---
+++ Modules.js
@@ -0,0 +1,13 @@
+//Modules 
+// Understanding = modules is a peace of code (minimum value ) , 
+//can be used in the file once it is assigned
+
+
+const first = `john`;
+const dinesh = `dinesh`
+
+let sayHI = (name)=>{
+    console.log(`say hi to ${name}`);
+}
+sayHI(`roko`)
+sayHI(first)
