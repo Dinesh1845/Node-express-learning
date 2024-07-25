1bc6e5cf7bfb01133751b961295105b423a287a5
DineshShankar <dineshshankar156@gmail.com>
2024-07-25 20:02:42 +0530

    c2

--- 1 -Modules.js
+++ 1 -Modules.js
@@ -3,11 +3,10 @@
 //can be used in the file once it is assigned
 
 
-const first = `john`;
-const dinesh = `dinesh`
 
 let sayHI = (name)=>{
     console.log(`say hi to ${name}`);
 }
-sayHI(`roko`)
-sayHI(first)
+
+const namess = require(`./example`)
+sayHI(namess);
